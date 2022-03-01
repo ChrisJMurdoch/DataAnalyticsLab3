78d46ce604f2eeb7cd19ddeed9fb5f72561c4ebe
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2022-03-01 02:02:42 +0000

    Added map

--- scripts/map.js
+++ scripts/map.js
@@ -1,10 +1,25 @@
 
-// Data sources
-const africaJson = "https://assets.codepen.io/2814973/africa.json"; // From lecture slides
-const worldJson = "https://raw.githubusercontent.com/ChrisJMurdoch/DataAnalyticsLab3/master/data/countries.geojson"; // From https://datahub.io/core/geo-countries#resource-countries
+
+// Natural Earth data in JSON format from https://github.com/martynafford/natural-earth-geojson/tree/master/50m/cultural
+const worldJson = "https://raw.githubusercontent.com/martynafford/natural-earth-geojson/master/50m/cultural/ne_50m_admin_0_countries.json";
+
+// Create offsets to centre map on Europe (Excluding Antarctica)
+const mapOffset = 1.4,
+      aspectRatio = 1.5;
+
+// Display size
+const width = d3.select("#canvas").style("width").slice(0, -2),
+      height = width / aspectRatio;
+
+// Set canvas height
+d3.select("#canvas").style("height", height);
 
 // Create GeoGenerator
-const projection = d3.geoMercator().scale(400).translate([200, 280]).center([0, 5]);
+const projection = d3.geoMercator()
+    .scale(width/Math.PI/2)
+    .rotate([-10, 0])
+    .center([0, 0])
+    .translate([width/2, mapOffset*height/2]);
 const geoGenerator = d3.geoPath().projection(projection);
 
 // Handle mouseover on regions
@@ -17,14 +32,10 @@ function handleMouseover(event, data) {
 
     // Display region-box data
     d3.select('#info')
-        .text(`${data.properties.name}. Area: ${pixelArea.toFixed(1)} Measure: ${measure.toFixed(1)})`);
-
-    // Attach bounding-box to region-box
-    d3.select('#bounding-box')
-        .attr('x', bounds[0][0])
-        .attr('y', bounds[0][1])
-        .attr('width', bounds[1][0] - bounds[0][0])
-        .attr('height', bounds[1][1] - bounds[0][1]);
+        .text(`${data.properties.NAME} | Area: ${pixelArea.toFixed(1)} | Measure: ${measure.toFixed(1)})`);
+    
+    // Bring region to front
+    d3.select("#foreground_region").attr("xlink:href", `#${data.properties.NAME.replaceAll(" ", "_")}`)
 }
 
 // Load data into map
@@ -36,6 +47,7 @@ d3.json(worldJson).then(function(json) {
         .data(json.features)
         .enter()
         .append('path')
+        .attr('id', (d) => `${d.properties.NAME.replaceAll(" ", "_")}`)
         .attr("class", "region")
         .attr('d', geoGenerator)
         .on('mouseover', handleMouseover);
